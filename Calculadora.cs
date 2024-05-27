892af56496607a49c5218a7d9ebb080dac2bdbf7
Santiago Z <test@gmail.com>
2024-05-27 17:05:03 -0300

    punto 1 terminado

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,37 +1,32 @@
-namespace EspacioCalculadora;
-
-public class Calculadora(){
-    private int n1;
-    public int N1{
-        get;
-        set;
-    }
-    private int n2;
-    public int N2{
-        get;
-        set;
-    }
-        
-    private float resultado;
-    public float R{
-        get;
-        set;
-    }
-    int sum, res, mul, div;
-
-    void sumar(){
-        sum = n1 + n2;
-    }
-    void restar(){
-        res = n1 - n2;
-    }
-    void multiplicar(){
-        mul = n1 * n2;
-    }
-    void dividir(){
-        div = n1 / n2;
+namespace EspacioCalculadora{
+    public class Calculadora(){
+        private int n1;
+        public int N1{
+            get => n1;
+            set => n1 = value;
+        }
+        private int n2;
+        public int N2{
+            get => n2;
+            set => n2 = value;
+        }
+            
+        private float resultado;
+        public float R{
+            get => resultado;
+            set => resultado = value;
+        }
+        public void sumar(){
+            resultado = N1 + N2;
+        }
+        public void restar(){
+            resultado = N1 - N2;
+        }
+        public void multiplicar(){
+            resultado = N1 * N2;
+        }
+        public void dividir(){
+            resultado = N1 / N2;
+        }
     }
 }
-
-Calculadora calc1 = new Calculadora();
-calc1.sumar(2.3);
--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EspacioCalculadora;
+
+Calculadora calc1 = new Calculadora();
+
+//Calculadora V1 full string
+int bandera = 0;
+int num1;
+int num2;
+
+do{
+    Console.WriteLine("--- CALCULADORA FULL STRING ---");
+    Console.WriteLine("Ingrese una operacion matematica");
+    string opMat = Console.ReadLine();
+
+    if(opMat.IndexOf("+", StringComparison.OrdinalIgnoreCase)>=0){
+        string[] op = opMat.Split('+');       
+        int.TryParse(op[0], out num1);
+        int.TryParse(op[1], out num2);
+        calc1.N1 = num1;
+        calc1.N2 = num2;
+        calc1.sumar();
+        Console.WriteLine("La suma es: " + calc1.R);     
+    }else if(opMat.IndexOf("-", StringComparison.OrdinalIgnoreCase)>=0){
+        string[] op = opMat.Split('-');        
+        int.TryParse(op[0], out num1);
+        int.TryParse(op[1], out num2);
+        calc1.N1 = num1;
+        calc1.N2 = num2;
+        calc1.restar();
+        Console.WriteLine("La diferencia es: " + calc1.R);
+    }else if(opMat.IndexOf("*", StringComparison.OrdinalIgnoreCase)>=0){
+        string[] op = opMat.Split('*');  
+        int.TryParse(op[0], out num1);
+        int.TryParse(op[1], out num2);      
+        calc1.N1 = num1;
+        calc1.N2 = num2;
+        calc1.multiplicar();
+        Console.WriteLine("El producto es: "+ calc1.R);
+    }else if(opMat.IndexOf("/", StringComparison.OrdinalIgnoreCase)>=0){
+        string[] op = opMat.Split('/');  
+        int.TryParse(op[0], out num1);
+        int.TryParse(op[1], out num2);    
+        calc1.N1 = num1;
+        calc1.N2 = num2;
+        if(calc1.N2 != 0){
+            calc1.dividir(); 
+            Console.WriteLine("El cociente es: "+ calc1.R); 
+        }else{
+            Console.WriteLine("No se puede dividir en 0"); 
+        }
+    }else{
+        Console.WriteLine("Ingrese una operacion valida");
+    }
+    Console.WriteLine("Quiere realizar otra operacion? 1:Si / 0: No");
+    string buff3 = Console.ReadLine();
+    int.TryParse(buff3, out bandera);
+}while(bandera==1);
